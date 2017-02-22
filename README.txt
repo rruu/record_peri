30d0d151196326293be4d296037d84b77d8856cb
Peterf <peter@jonge.nu>
2017-02-22 22:37:24 +0100

    support twitter accountnames

--- README.txt
+++ README.txt
@@ -7,9 +7,10 @@ Requirements:
 - ffmpeg
 	https://ffmpeg.org/
 
-For users in the csv you need the Periscope account name.
-e.g. @abc123 is user abc123.
-A Twitter account name doesn't work.
+For users in the csv you need the Periscope or Twiter account name.
+e.g. @abc123 is user abc123:p.
+p = Periscope account name
+t = Twitter account name
 
 Because ffmpeg can't stop decent with a script, the mkv isn't closed correctly.
 At the end the mkv is converted to a good working mp4 file.
--- record_peri/record_peri.py
+++ record_peri/record_peri.py
@@ -5,6 +5,9 @@
 # record_peri.py and csv file.
 # It is possible te edit the csv file while record_peri.py is running.
 # Use Notepad++ for editing.
+# Format csv: abc123:p,johndoe:p,xyzxx:t
+# p = Periscope account name (user uses Pericope to stream)
+# t = Twitter account name (user uses Twitter to stream)
 #
 # Requirements:	- Python 3
 #				- ffmpeg
@@ -17,6 +20,7 @@ import json
 import urllib.request, urllib.error
 
 PERISCOPE_URL = 'https://www.periscope.tv/'
+TWITTER_URL = 'https://twitter.com/'
 HLSURL1 = [
 	'https://periscope-prod-eu-central-1.global.ssl.fastly.net/vidmanlive/',
 	'https://periscope-prod-sa-east-1.global.ssl.fastly.net/vidmanlive/',
@@ -56,35 +60,72 @@ def get_live_broadcast(user):
 				else:
 					live_broadcast = {}	
 	except urllib.error.URLError as e:
-		print("URLError: ",e.reason)
+		print('URLError: ',e.reason)
 		res = e.reason
-		if res == "Not Found":
+		if res == 'Not Found':
 			live_broadcast = {'user_id': ['unknown']}
 		else:
 			#unknown error
 			live_broadcast = {'user_id': ['skip']}
 	return live_broadcast
 	
-def rec_ffmpeg(user, input, output):
+def get_twitter_streamURL(user):
+	req = urllib.request.Request(TWITTER_URL + user)
+	try:
+		response = urllib.request.urlopen(req)
+		r = response.read()
+		soup = BeautifulSoup(r, 'html.parser')
+		stream_container = str(soup.find(id="stream-items-id"))
+		if not stream_container.find('https://www.periscope.tv/w/') == -1:
+			streamURL = (stream_container[stream_container.find('https://www.periscope.tv/w/')+25:])
+			streamURL = (streamURL[:streamURL.find('" ')])
+		else:
+			#no streams or recorded streams
+			streamURL = 'nothing'
+	except urllib.error.URLError as e:
+		print('URLError: ',e.reason)
+		res = e.reason
+		if res == 'Not Found':
+			streamURL = 'unknown'
+		else:
+			#unknown error
+			streamURL = 'nothing'
+	return streamURL
+	
+def rec_ffmpeg(usershort, input, output):
 	command = ['ffmpeg.exe','-i' , input,'-y','-acodec','mp3','-loglevel','0', output]
-	p[user]=subprocess.Popen(command)
+	p[usershort]=subprocess.Popen(command)
 	broadcastdict[user]['recording'] = 1
 	time.sleep(1)
 
 while True:
 	#read users.csv into list every loop, so you can edit csv file during run.
-	with open("users.csv", 'r') as readfile:
+	with open('users.csv', 'r') as readfile:
 		reader = csv.reader(readfile, delimiter=',')
 		usernames2 = list(reader)
 	usernames = usernames2[0]
 	
 	deleteuserbroadcast = []
 	for user in usernames:
-		print ("Poll ", user)
-		live_broadcast = get_live_broadcast(user)
+		usershort = user[:-2]
+		usertype = user[-1:]
+		#Peri or Twitter user
+		if usertype == 't':
+			streamURL = get_twitter_streamURL(usershort)
+			print ('Pol Twitter account: ', usershort)
+			if streamURL == 'unknown':
+				#user does not exists
+				live_broadcast = get_live_broadcast(usershort)
+			elif streamURL == 'nothing':
+				live_broadcast = {}
+			else:
+				live_broadcast = get_live_broadcast(streamURL)
+		else:
+			print ('Pol Peri account   : ', usershort)
+			live_broadcast = get_live_broadcast(usershort)
 		if not live_broadcast:
 			if user in broadcastdict:
-				print (user, ': broadcast ended')
+				print (usershort, ': broadcast ended')
 				deleteuserbroadcast.append(user)
 				broadcastdict[user]['state'] = 'ENDED'
 		elif live_broadcast['user_id'] == ['unknown']:
@@ -95,26 +136,26 @@ while True:
 					deleteuserbroadcast.append(user)
 				usernames.remove(user)
 				deleteuser.remove(user)
-				print ("Delete user: ", user)
-				with open("users.csv", 'w') as outfile:
+				print ('Delete user: ', usershort)
+				with open('users.csv', 'w') as outfile:
 					writer = csv.writer(outfile, delimiter=',',quoting=csv.QUOTE_ALL)
 					writer.writerow(usernames)
 			else:
 				deleteuser.append(user)
-				print ("Loop delete user: ", user)
+				print ('Loop delete user: ', usershort)
 			break
 		elif live_broadcast['user_id'] == ['skip']:
 			#skip user loop
-			print ("HTTP request error. Skip user: ", user)
+			print ('HTTP request error. Skip user: ', usershort)
 		else:
 			if user not in broadcastdict:
-				print ("New scope of user: ", user)
+				print ('New scope of user: ', usershort)
 				broadcast_id = live_broadcast['id']
 				HLS_URL_2 = live_broadcast['image_url'][24:]
-				if "chunk" in HLS_URL_2:
+				if 'chunk' in HLS_URL_2:
 					chunkpos = HLS_URL_2.find('chunk') - 1
 					HLS_URL_2 = HLS_URL_2[:chunkpos]
-				if "orig.jpg" in HLS_URL_2:
+				if 'orig.jpg' in HLS_URL_2:
 					chunkpos = HLS_URL_2.find('orig.jpg') - 1
 					HLS_URL_2 = HLS_URL_2[:chunkpos]
 				broadcastdict[user] = {}
@@ -122,7 +163,7 @@ while True:
 				broadcastdict[user]['HLS_URL']= HLS_URL_2 + HLS_URL_3
 				broadcastdict[user]['state']= 'RUNNING'
 				broadcastdict[user]['time']= time.time()
-				broadcastdict[user]['filename']= user + '_on_peri_' + str(broadcastdict[user]['time'])[:10] + '.mkv'
+				broadcastdict[user]['filename']= usershort + '_on_peri_' + str(broadcastdict[user]['time'])[:10] + '.mkv'
 				broadcastdict[user]['filesize']= 0
 				broadcastdict[user]['recording']= 0
 				broadcastdict[user]['streamerror'] = 0
@@ -132,40 +173,40 @@ while True:
 			if broadcastdict[user]['filesize'] < file_size(broadcastdict[user]['filename']):
 				broadcastdict[user]['filesize'] = file_size(broadcastdict[user]['filename'])
 				broadcastdict[user]['streamerror'] = 0
-				print ("Running recording: ", broadcastdict[user]['filename'])
+				print ('Running recording: ', broadcastdict[user]['filename'])
 			else:
 				if broadcastdict[user]['streamerror'] == 1:
 					#extra loop
-					print ("End recording for: ", broadcastdict[user]['filename'] , " :stream error")
+					print ('End recording for: ', broadcastdict[user]['filename'] , ' :stream error')
 					deleteuserbroadcast.append(user)
 				else:
 					broadcastdict[user]['streamerror'] = 1
 	#start stop recordings
 	for user in broadcastdict:
 		if broadcastdict[user]['recording'] == 0 and broadcastdict[user]['state'] == 'RUNNING':
-			print ("Start recording for: ", user)
+			print ('Start recording for: ', usershort)
 			for key in HLSURL1:
 				URL = key + broadcastdict[user]['HLS_URL']
-				rec_ffmpeg(user, URL, broadcastdict[user]['filename'] )
+				rec_ffmpeg(usershort, URL, broadcastdict[user]['filename'] )
 				if os.path.exists(broadcastdict[user]['filename']):
 					print ('Recording started from: ', key)
 					break
 				else:
-					p[user].terminate()
+					p[usershort].terminate()
 			if not os.path.exists(broadcastdict[user]['filename']):
-				print ("No recording file created for: ", user, "file: ", broadcastdict[user]['filename'])
+				print ('No recording file created for: ', usershort, 'file: ', broadcastdict[user]['filename'])
 				deleteuserbroadcast.append(user)
 		if broadcastdict[user]['state'] == 'ENDED' and broadcastdict[user]['recording'] == 1:
 			# end recording
-			print ("End recording for: ", user)
+			print ('End recording for: ', user)
 			deleteuserbroadcast.append(user)
 	#end recording, delete entry in broadcastdict and convert mkv -> mp4
 	for user in deleteuserbroadcast:
-		p[user].terminate()
+		p[usershort].terminate()
 		if user in broadcastdict:
 			input = broadcastdict[user]['filename']
 			output = input.replace('.mkv','.mp4')
 			command = ['ffmpeg.exe','-i' , input,'-y','-loglevel','0', output]
-			#p1[user]=subprocess.Popen(command)
+			p1[user]=subprocess.Popen(command)
 			del broadcastdict[user]
 	time.sleep(1)
