0697b86af93635d9173010dc8778417f272d8bae
Peterf <peter@jonge.nu>
2017-03-31 17:08:28 +0200

    record start delay. removed old HLSURL

--- record_peri/record_peri.py
+++ record_peri/record_peri.py
@@ -50,15 +50,7 @@ HLSURL1 = {
 	'https://prod-video-ap-southeast-1.periscope.tv/':'/live/ap-southeast-1/playlist.m3u8',
 	'https://prod-video-us-west-1.periscope.tv/':'/live/us-west-1/playlist.m3u8',
 	'https://prod-video-us-east-1.periscope.tv/':'/live/us-east-1/playlist.m3u8',
-	'https://prod-video-sa-east-1.periscope.tv/':'/live/sa-east-1/playlist.m3u8',
-	'https://periscope-prod-eu-central-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8',
-	'https://periscope-prod-eu-west-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8',
-	'https://periscope-prod-ap-northeast-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8',
-	'https://periscope-prod-ap-southeast-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8',
-	'https://periscope-prod-us-west-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8',
-	'https://periscope-prod-us-east-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8',
-	'https://periscope-prod-sa-east-1.global.ssl.fastly.net/vidmanlive/':'/playlist.m3u8'
-	
+	'https://prod-video-sa-east-1.periscope.tv/':'/live/sa-east-1/playlist.m3u8'
 	}
 broadcastdict = {}
 deleteuser = []
@@ -66,7 +58,6 @@ p = {}
 p1 = {}
 
 def file_size(fname):
-        import os
         statinfo = os.stat(fname)
         return statinfo.st_size
 
@@ -202,10 +193,10 @@ while True:
 					for key in HLSURL1:
 						URL = key + broadcastdict[user]['HLS_URL2'] + HLSURL1[key]
 						rec_ffmpeg(user, URL, broadcastdict[user]['filename'] )
+						time.sleep(1)
 						if os.path.exists(broadcastdict[user]['filename']):
 							print ('Recording started from: ', key)
 							broadcastdict[user]['HLS_URL'] = URL
-							time.sleep(2)
 							break
 						else:
 							p[user].terminate()
